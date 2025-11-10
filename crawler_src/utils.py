e46e2b4a9c22e492fb16fa8595f61d40ff510cc0
Najeh Halawani <n.helwani1@gmail.com>
2025-11-10 12:19:51 +0100

    fix new directories creation location

--- crawler_src/utils.py
+++ crawler_src/utils.py
@@ -1,7 +1,7 @@
 '''
 Author: Najeh Halawani
 -----
-Last Modified: Monday, 10th November 2025 12:16:04 pm
+Last Modified: Monday, 10th November 2025 12:18:34 pm
 Modified By: Najeh Halawani
 -----
 '''
@@ -26,13 +26,14 @@ words = word_data["words"]
 
 
 def setup_logging():
-    # folders = ['crawl_data_block ', 'crawl_data_accept', 'crawl_data_reject', 'analysis', 'crawler_src' ]
-    # for folder in folders:
-    #     if not os.path.exists(folder):
-    #         os.makedirs(folder)
-    #         logging.info(f"Created folder: {folder}")
+    parent_dir = Path(__file__).parent.parent
+    folders = ['crawl_data_block ', 'crawl_data_accept', 'crawl_data_reject', 'analysis', 'crawler_src' ]
+    for folder in folders:
+        folder_path = parent_dir / folder
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            logging.info(f"Created folder: {folder_path}")
             
-
     log_dir = Path("logs")
     log_dir.mkdir(exist_ok=True)
     
